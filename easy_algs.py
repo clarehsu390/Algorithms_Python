003715e1902f3627e8944e1c711ea5960314c543
Clare Hsu <clare.hsu390@gmail.com>
2018-01-10 13:20:28 -0800

    Implement fibonacci

--- easy_algs.py
+++ easy_algs.py
@@ -25,4 +25,25 @@ def prime_factors(num):
 
 # print prime_factors(63)
         
+def fibonacci(n):
+    fib_nums = [0, 1]
+    if n <= 2:
+        return fib_nums[0:n]
+    i = 2
+    while i < n:
+        new_num = fib_nums[i-1] + fib_nums[i-2]
+        fib_nums.append(new_num)
+        i += 1
+    
+    return fib_nums
 
+print fibonacci(5)
+print fibonacci(10)
+
+def fib_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return fib_recursive(n-1) + fib_recursive(n-2)
+
+print fib_recursive(5)
